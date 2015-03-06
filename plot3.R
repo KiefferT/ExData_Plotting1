6729d2269f2dc9f5e1e2e285e1592f138654bf32
KiefferT <thomas.kieffer4@gmail.com>
2015-03-06 12:45:49 -0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -15,11 +15,11 @@ plot3 <- function() {
     png(file = "plot3.png", width = 480, height = 480)
 
     ## Create plot
-    plot(data3$fulldate, data3$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+    plot(data$fulldate, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
     par(new = T)
-    plot(data3$fulldate, data3$Sub_metering_2, type = "l", col = "red", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n")
+    plot(data$fulldate, data$Sub_metering_2, type = "l", col = "red", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n")
     par(new = T)
-    plot(data3$fulldate, data3$Sub_metering_3, type = "l", col = "blue", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n")
+    plot(data$fulldate, data$Sub_metering_3, type = "l", col = "blue", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n")
     
     ## Add legend
     legend("topright", lty=c(1,1,1), col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
