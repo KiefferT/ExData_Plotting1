e699dc7c266fe3d54f9918973d75b8c3a1c195af
Kieffer Thomas <thomas.kieffer4@gmail.com>
2015-03-06 14:01:52 -0800

    plots 3 and 4

--- plot4.R
+++ plot4.R
@@ -26,9 +26,9 @@ plot4 <- function() {
     ## Plot 3
     plot(data$fulldate, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
     par(new = T)
-    plot(data$fulldate, data$Sub_metering_2, type = "l", col = "red", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n")
+    plot(data$fulldate, data$Sub_metering_2, type = "l", col = "red", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n", xaxt = "n")
     par(new = T)
-    plot(data$fulldate, data$Sub_metering_3, type = "l", col = "blue", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n")
+    plot(data$fulldate, data$Sub_metering_3, type = "l", col = "blue", xlab = "", ylab = "", ylim = c(0, 40), yaxt = "n", xaxt = "n")
     legend("topright", lty=c(1,1,1), col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     
     ##Plot4
